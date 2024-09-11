8c1b84644a2f709123a5d7c58d443669688f22a1
Mlook70 <Abdulmalek.akel@gmail.com>
2024-09-11 06:02:07 +0300

    TEST VITE DEPLOY

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/IBM_Shopping_Cart_React/",
   plugins: [react()],
-  base: "/IBM_Shopping_Cart_React"
 })
