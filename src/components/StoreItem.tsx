626f3453ee5f1982b2539b295613ef71273a63c8
Mlook70 <Abdulmalek.akel@gmail.com>
2024-09-11 04:16:28 +0300

    Last Update

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -29,7 +29,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span className="fs-2 text-white">{name}</span>
-          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+          <span className="ms-2 text-white">{formatCurrency(price)}</span>
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
